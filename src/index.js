56627df8c20e605fbc4dee74ce711d14eaa80e04
Abdelrahman <elhedody@gmail.com>
2019-09-25 14:27:11 +0200

    done

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Main from "./components/Main";
+import "./style/stylesheet.css";
+
+ReactDom.render(<Main />, document.getElementById("root"));
